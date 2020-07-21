24dc46d08dddaead6357a9000076dc68f959515a
eshafik <shafik.te@gmail.com>
2020-07-21 11:46:36 +0600

    Updates

---
+++ precache-manifest.6103153c51b1d6dc1e0f2e222ef53602.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40de1b014d9b77564bec8ccf8bcbf41",
+    "url": "/corona-test/index.html"
+  },
+  {
+    "revision": "df7b5d7801105f25c4d1",
+    "url": "/corona-test/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c8b3c40b6770b345bf60",
+    "url": "/corona-test/static/css/main.21ea3bdc.chunk.css"
+  },
+  {
+    "revision": "df7b5d7801105f25c4d1",
+    "url": "/corona-test/static/js/2.02dabf61.chunk.js"
+  },
+  {
+    "revision": "14c54f8d621b663941f58f815237ec03",
+    "url": "/corona-test/static/js/2.02dabf61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b3c40b6770b345bf60",
+    "url": "/corona-test/static/js/main.ed8f365f.chunk.js"
+  },
+  {
+    "revision": "92b8d480165383e2acd0",
+    "url": "/corona-test/static/js/runtime-main.cf0e86d7.js"
+  },
+  {
+    "revision": "290629745448cd599d0517e1299c6b4e",
+    "url": "/corona-test/static/media/corona-bg.29062974.jpg"
+  },
+  {
+    "revision": "f6336cb3255f0bda14a21cc28ee175a4",
+    "url": "/corona-test/static/media/shafik.f6336cb3.jpg"
+  }
+]);
